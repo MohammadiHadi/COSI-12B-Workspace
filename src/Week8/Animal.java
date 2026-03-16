da7c826ec2b6d7aaef3d4f53f457a69f2cf41734
MohammadiHadi <122750330+MohammadiHadi@users.noreply.github.com>
2026-03-16 13:18:17 -0400

    Adding new codes

---
+++ src/Week8/Animal.java
@@ -0,0 +1,12 @@
+package Week8;
+
+public class Animal { 
+     int health = 100; 
+} 
+class Mammal extends Animal { 
+} 
+class Cat extends Mammal { 
+} 
+class Dog extends Mammal { 
+} 
+
---
+++ src/Week8/Test.java
@@ -0,0 +1,15 @@
+package Week8;
+
+public class Test { 
+    public static void main(String[] args) { 	
+        // Cat c = new Cat(); 	System.out.println(c.health); 
+	    // Dog d = new Dog(); 	System.out.println(d.health); 
+
+        Cat c = new Cat();
+    	System.out.println(c);
+    	Mammal m = c; // upcasting
+    	System.out.println(m);
+
+    } 
+}
+
