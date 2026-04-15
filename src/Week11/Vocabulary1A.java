7e4ce6fd6b0556879b12414526bca2e99f1fb097
MohammadiHadi <122750330+MohammadiHadi@users.noreply.github.com>
2026-04-15 13:24:42 -0400

    Adding new codes

--- src/Week11/Vocabulary1A.java
+++ src/Week11/Vocabulary1A.java
@@ -11,11 +11,19 @@ public class Vocabulary1A {
         Scanner input2 = new Scanner(new File("test2.txt"));
         ArrayList<String> list1 = getWords(input1);
         ArrayList<String> list2 = getWords(input2);
+        ArrayList<String> overlap = getOverlap(list1, list2);
+
+
         System.out.println("list1 = " + list1);
         System.out.println("list2 = " + list2);
+        System.out.println("overlap = " + overlap);
+        reportResults(list1, list2, overlap);
+
     }
 
     public static ArrayList<String> getWords(Scanner input) {
+
+        input.useDelimiter("[^a-zA-Z']+");
         // ArrayList<String> words = new ArrayList<String>();
         // while (input.hasNext()) {
         //     String next = input.next().toLowerCase();
@@ -46,6 +54,43 @@ public class Vocabulary1A {
        return result;
 
     }
+
+
+    public static ArrayList<String> getOverlap(ArrayList<String> list1, ArrayList<String> list2) {
+	ArrayList<String> result = new ArrayList<String>();
+	int index1 = 0;
+	int index2 = 0;
+	while (index1 < list1.size() && index2 < list2.size()) {
+		int comparison= list1.get(index1).compareTo(list2.get(index2));
+		if (comparison == 0) {
+		     result.add(list1.get(index1));
+		     index1++;
+     	     index2++;
+		} else if (comparison < 0) {
+		     index1++;
+         } else {  // comparison > 0
+              index2++;
+         }
+	}
+	return result;
+}
+
+// pre : overlap contains the words in common between list1 and list2
+// post: reports statistics about two-word lists and their overlap
+public static void reportResults(ArrayList<String> list1,
+             ArrayList<String> list2, ArrayList<String> overlap) {
+      System.out.println("file #1 words = " + list1.size());
+      System.out.println("file #2 words = " + list2.size());
+      System.out.println("common words  = " + overlap.size());
+
+      double percent1 = 100.0 * overlap.size() / list1.size();
+      double percent2 = 100.0 * overlap.size() / list2.size();
+      System.out.println("% of file 1 in overlap = " + percent1);
+      System.out.println("% of file 2 in overlap = " + percent2);
+}
+
+
+
 }
 
 
